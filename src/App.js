6f599331840746cc65c270cdf239b799cdeb97bb
julius_ek_hub <ekanejulius7@gmail.com>
2022-08-21 12:46:06 +0400

    cd: changed live test server form vercel to heroku and implemented force http over https to be able to talk to insecure api

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useEffect } from "react";
+
 import { ThemeProvider } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 
@@ -12,6 +14,17 @@ import useExtra from "./hooks/useExtra";
 
 function App() {
 	const { theme: mode } = useExtra();
+
+	//  You can remove this hook in your local environment
+	//  ---------------------------------------------
+	//  Since the api endpoints are not secured, the live preview on heroku must be forced over http
+	//  to be able to talk to the the api
+
+	useEffect(() => {
+		const location = window.location;
+		if (location.protocol !== "http:")
+			location.href = "http://" + location.host;
+	}, []);
 
 	return (
 		<ThemeProvider theme={theme(mode)}>
